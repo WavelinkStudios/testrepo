c886f70de4599845fa81c94bc06a62d2e0c08ad2
TD <t>
2019-01-18 14:55:33 -0500

    Missed files somehow?

---
+++ depictions/com.wavelink.bundleidsxi/js/app-1.js
@@ -0,0 +1,9 @@
+var app = new Framework7({
+  root:'#app',
+  theme:'auto',
+  routes:routes
+});
+
+app.views.create('.view-main',{
+  url:'/'
+});
---
+++ depictions/com.wavelink.bundleidsxi/js/routes.js
@@ -0,0 +1,41 @@
+var routes = [
+  // Index page
+  {
+    path: '/',
+    componentUrl: './testdepiction.html',
+    routes: [
+      {
+        path: 'changes/',
+        url: './changelog.html',
+      },
+    ],
+  },
+
+  // Page Loaders
+  {
+    path: '/page-loader-template7/:user/:userId/:posts/:postId/',
+    templateUrl: './pages/page-loader-template7.html',
+    // additional context
+    options: {
+      context: {
+        foo: 'bar',
+      },
+    },
+  },
+  {
+    path: '/page-loader-component/:user/:userId/:posts/:postId/',
+    componentUrl: './pages/page-loader-component.html',
+    // additional context
+    options: {
+      context: {
+        foo: 'bar',
+      },
+    },
+  },
+
+  // Default route (404 page). MUST BE THE LAST
+  {
+    path: '(.*)',
+    url: './pages/404.html',
+  },
+];
